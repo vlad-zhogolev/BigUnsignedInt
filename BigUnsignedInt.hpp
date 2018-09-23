91a794fef8c94890c7be1eaf3aa68892d1fa3c95
zvladyuha <z.vladyuha@yandex.ru>
2018-09-24 01:04:55 +0300

    made code more readable

--- BigUnsignedInt.hpp
+++ BigUnsignedInt.hpp
@@ -9,7 +9,6 @@
 #include <vector>
 #include <regex>
 #include <queue>
-#include <sstream>
 
 const std::size_t BIG_UNSIGNED_INT_SIZE = 64;
 
@@ -37,6 +36,7 @@ public:
 
     using Unsigned = unsigned int;
     using UnsignedVector = std::vector<Unsigned>;
+    using size_type = UnsignedVector::size_type;
 
     friend std::istream& operator>><N>(std::istream& is, BigUnsignedInt<N>& bigUnsignedInt);
 
@@ -52,15 +52,15 @@ public:
 
     BigUnsignedInt() : _binRepr(N) {}
 
-    static bool overflowed() { return _overflowBit > 0; }
+    static bool overflowed() { return _carryFlag > 0; }
 
 private:
     UnsignedVector _binRepr;
-    static Unsigned _overflowBit;
+    static Unsigned _carryFlag;
 };
 
 template<size_t N>
-typename BigUnsignedInt<N>::Unsigned BigUnsignedInt<N>::_overflowBit = 0;
+typename BigUnsignedInt<N>::Unsigned BigUnsignedInt<N>::_carryFlag = 0;
 
 template<std::size_t N>
 std::istream& operator>>(std::istream& is, BigUnsignedInt<N>& bigUnsignedInt)
@@ -75,14 +75,11 @@ std::istream& operator>>(std::istream& is, BigUnsignedInt<N>& bigUnsignedInt)
 
     // Fill queue with digits of provided number
     queue<typename BigUnsignedInt<N>::Unsigned> digits;
-    //deque<BigUnsignedInt::Unsigned> digits;
     for (const auto& letter:input)
         digits.push(static_cast<typename BigUnsignedInt<N>::Unsigned>(letter - '0'));
 
     deque<typename BigUnsignedInt<N>::Unsigned> quotient;
-    typename BigUnsignedInt<N>::UnsignedVector::size_type counter = N - 1;
-    //deque<BigUnsignedInt::Unsigned>* quotPtr = &quotient;
-    //deque<BigUnsignedInt::Unsigned>* divPtr = &digits;
+    typename BigUnsignedInt<N>::size_type counter = N - 1;
 
     // Convert provided number to binary
     do
@@ -116,7 +113,7 @@ std::istream& operator>>(std::istream& is, BigUnsignedInt<N>& bigUnsignedInt)
 template<std::size_t N>
 std::ostream& operator<<(std::ostream& os, const BigUnsignedInt<N>& bigUnsignedInt)
 {
-    typename BigUnsignedInt<N>::UnsignedVector::size_type counter = 1;
+    typename BigUnsignedInt<N>::size_type counter = 1;
     for (const auto& bit:bigUnsignedInt._binRepr)
     {
         os << bit;
@@ -132,14 +129,14 @@ std::ostream& operator<<(std::ostream& os, BigUnsignedInt<N>&& bigUnsignedInt) {
 template<std::size_t N>
 BigUnsignedInt<N> operator+(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b)
 {
-    BigUnsignedInt<N>::_overflowBit = 0;
+    BigUnsignedInt<N>::_carryFlag = 0;
     BigUnsignedInt<N> result;
-    for (typename BigUnsignedInt<N>::UnsignedVector::size_type i = N - 1;
-         i != std::numeric_limits<typename BigUnsignedInt<N>::UnsignedVector::size_type>::max(); --i)
+    for (typename BigUnsignedInt<N>::size_type i = N - 1;
+         i != std::numeric_limits<typename BigUnsignedInt<N>::size_type>::max(); --i)
     {
-        result._binRepr[i] = a._binRepr[i] ^ b._binRepr[i] ^ BigUnsignedInt<N>::_overflowBit;
-        BigUnsignedInt<N>::_overflowBit =
-                (a._binRepr[i] & b._binRepr[i]) | (BigUnsignedInt<N>::_overflowBit & (a._binRepr[i] ^ b._binRepr[i]));
+        result._binRepr[i] = a._binRepr[i] ^ b._binRepr[i] ^ BigUnsignedInt<N>::_carryFlag;
+        BigUnsignedInt<N>::_carryFlag =
+                (a._binRepr[i] & b._binRepr[i]) | (BigUnsignedInt<N>::_carryFlag & (a._binRepr[i] ^ b._binRepr[i]));
     }
     return result;
 }
@@ -147,15 +144,15 @@ BigUnsignedInt<N> operator+(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>&
 template<std::size_t N>
 BigUnsignedInt<N> operator-(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b)
 {
-    BigUnsignedInt<N>::_overflowBit = 0;
+    BigUnsignedInt<N>::_carryFlag = 0;
     BigUnsignedInt<N> result;
-    for (typename BigUnsignedInt<N>::UnsignedVector::size_type i = N - 1;
-         i != std::numeric_limits<typename BigUnsignedInt<N>::UnsignedVector::size_type>::max(); --i)
+    for (typename BigUnsignedInt<N>::size_type i = N - 1;
+         i != std::numeric_limits<typename BigUnsignedInt<N>::size_type>::max(); --i)
     {
-        result._binRepr[i] = a._binRepr[i] ^ b._binRepr[i] ^ BigUnsignedInt<N>::_overflowBit;
-        BigUnsignedInt<N>::_overflowBit =
+        result._binRepr[i] = a._binRepr[i] ^ b._binRepr[i] ^ BigUnsignedInt<N>::_carryFlag;
+        BigUnsignedInt<N>::_carryFlag =
                 ((1 ^ a._binRepr[i]) & b._binRepr[i]) |
-                (BigUnsignedInt<N>::_overflowBit & ((1 ^ a._binRepr[i]) ^ b._binRepr[i]));
+                (BigUnsignedInt<N>::_carryFlag & ((1 ^ a._binRepr[i]) ^ b._binRepr[i]));
     }
     return result;
 }
--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 
 using namespace std;
 
-
 int main()
 {
     BigUnsignedInt<64> bi1, bi2;
     cin >> bi1;
     cin >> bi2;
     BigUnsignedInt<64> bi3 = bi1 + bi2;
-    cout << bi1 << "\n";
-    cout << bi2 << "\n";
-    cout << bi3 << endl;
+    cout << bi1 << "\n"
+         << bi2 << "\n"
+         << bi3 << endl;
     return 0;
 }
