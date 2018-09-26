61d8dcff84867fb97cf30b7f6f72d3a2aeb56e5c
zvladyuha <z.vladyuha@yandex.ru>
2018-09-26 13:28:52 +0300

    added shift and multiplication operators, removed redundant friend declarations

--- BigUnsignedInt.hpp
+++ BigUnsignedInt.hpp
@@ -9,7 +9,6 @@
 #include <vector>
 #include <regex>
 #include <queue>
-#include <sstream>
 
 const std::size_t BIG_UNSIGNED_INT_SIZE = 64;
 
@@ -26,10 +25,16 @@ template<std::size_t N>
 std::ostream& operator<<(std::ostream& os, BigUnsignedInt<N>&& bigUnsignedInt);
 
 template<std::size_t N>
+BigUnsignedInt<N> operator<<(BigUnsignedInt<N> bigUnsignedInt, std::size_t shift);
+
+template<std::size_t N>
 BigUnsignedInt<N> operator+(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b);
 
 template<std::size_t N>
 BigUnsignedInt<N> operator-(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b);
+
+template<std::size_t N>
+BigUnsignedInt<N> operator*(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b);
 
 template<std::size_t N = BIG_UNSIGNED_INT_SIZE>
 class BigUnsignedInt {
@@ -45,6 +50,8 @@ public:
 
     friend std::ostream& operator<<<N>(std::ostream& os, BigUnsignedInt&& bigUnsignedInt);
 
+    friend BigUnsignedInt operator<<<N>(BigUnsignedInt bigUnsignedInt, std::size_t shift);
+
     friend BigUnsignedInt operator+<N>(const BigUnsignedInt& a, const BigUnsignedInt& b);
 
     friend BigUnsignedInt operator-<N>(const BigUnsignedInt& a, const BigUnsignedInt& b);
@@ -55,9 +62,18 @@ public:
 
     explicit BigUnsignedInt(const std::string& stringRepr);
 
+    BigUnsignedInt& operator+=(const BigUnsignedInt& other);
+
+    BigUnsignedInt& operator-=(const BigUnsignedInt& other);
+
+    BigUnsignedInt& operator*=(const BigUnsignedInt& other);
+
+    BigUnsignedInt& operator<<=(std::size_t shift);
+
     static bool overflowed() { return _carryFlag > 0; }
 
 private:
+
     static void createFromString(const std::string& input, BigUnsignedInt& bigUnsignedInt);
 
 private:
@@ -102,32 +118,122 @@ std::ostream& operator<<(std::ostream& os, BigUnsignedInt<N>&& bigUnsignedInt)
 template<std::size_t N>
 BigUnsignedInt<N> operator+(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b)
 {
-    BigUnsignedInt<N>::_carryFlag = 0;
-    BigUnsignedInt<N> result;
-    for (typename BigUnsignedInt<N>::size_type i = N - 1;
-         i != std::numeric_limits<typename BigUnsignedInt<N>::size_type>::max(); --i)
-    {
-        result._binRepr[i] = a._binRepr[i] ^ b._binRepr[i] ^ BigUnsignedInt<N>::_carryFlag;
-        BigUnsignedInt<N>::_carryFlag =
-                (a._binRepr[i] & b._binRepr[i]) | (BigUnsignedInt<N>::_carryFlag & (a._binRepr[i] ^ b._binRepr[i]));
-    }
-    return result;
+    BigUnsignedInt<N> result(a);
+    return result += b;
 }
 
 template<std::size_t N>
 BigUnsignedInt<N> operator-(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b)
 {
-    BigUnsignedInt<N>::_carryFlag = 0;
-    BigUnsignedInt<N> result;
+    BigUnsignedInt<N> result(a);
+    return result -= b;
+}
+
+template<std::size_t N>
+BigUnsignedInt<N> operator*(const BigUnsignedInt<N>& a, const BigUnsignedInt<N>& b)
+{
+    BigUnsignedInt<N> result(a);
+    return result *= b;
+}
+
+template<std::size_t N>
+BigUnsignedInt<N> operator<<(BigUnsignedInt<N> bigUnsignedInt, const size_t shift)
+{
+    return bigUnsignedInt <<= shift;
+}
+
+//TODO: try to make it with Boolean function
+template<std::size_t N>
+BigUnsignedInt<N>& BigUnsignedInt<N>::operator<<=(const std::size_t shift)
+{
+    BigUnsignedInt::_carryFlag = 0;
+
+    // Shift completely erases number
+    if (shift >= N)
+    {
+        for (Unsigned& bit:_binRepr)
+            bit = 0;
+        BigUnsignedInt::_carryFlag = 1;
+        return *this;
+    }
+
+    // Find index of '1' appeared first from left in _binRepr
+    std::size_t firstOneIndex;
+    std::size_t index = 0;
+    while (index < N - 1 && _binRepr[index] != 1)
+        ++index;
+    firstOneIndex = index;
+
+    // Set the _carryFlag if we lose at least first '1' by applying shift
+    if (shift > firstOneIndex)
+        BigUnsignedInt<N>::_carryFlag = 1;
+
+    // Shift the number and fill right part of it with '0'
+    for (std::size_t i = shift; i < N; ++i)
+        _binRepr[i - shift] = _binRepr[i];
+    for (std::size_t i = N - shift; i < N; ++i)
+        _binRepr[i] = 0;
+
+    return *this;
+}
+
+template<std::size_t N>
+BigUnsignedInt<N>& BigUnsignedInt<N>::operator+=(const BigUnsignedInt& other)
+{
+    BigUnsignedInt::_carryFlag = 0;
     for (typename BigUnsignedInt<N>::size_type i = N - 1;
+         i != std::numeric_limits<typename BigUnsignedInt::size_type>::max(); --i)
+    {
+        Unsigned thisBit = this->_binRepr[i];
+        _binRepr[i] = thisBit ^ other._binRepr[i] ^ BigUnsignedInt::_carryFlag;
+        BigUnsignedInt::_carryFlag =
+                (thisBit & other._binRepr[i]) | (BigUnsignedInt::_carryFlag & (thisBit ^ other._binRepr[i]));
+    }
+    return *this;
+}
+
+template<std::size_t N>
+BigUnsignedInt<N>& BigUnsignedInt<N>::operator-=(const BigUnsignedInt& other)
+{
+    BigUnsignedInt::_carryFlag = 0;
+    for (typename BigUnsignedInt::size_type i = N - 1;
          i != std::numeric_limits<typename BigUnsignedInt<N>::size_type>::max(); --i)
     {
-        result._binRepr[i] = a._binRepr[i] ^ b._binRepr[i] ^ BigUnsignedInt<N>::_carryFlag;
-        BigUnsignedInt<N>::_carryFlag =
-                ((1 ^ a._binRepr[i]) & b._binRepr[i]) |
-                (BigUnsignedInt<N>::_carryFlag & ((1 ^ a._binRepr[i]) ^ b._binRepr[i]));
+        Unsigned thisBit = _binRepr[i];
+        _binRepr[i] = thisBit ^ other._binRepr[i] ^ BigUnsignedInt::_carryFlag;
+        BigUnsignedInt::_carryFlag =
+                ((1 ^ thisBit) & other._binRepr[i]) |
+                (BigUnsignedInt::_carryFlag & ((1 ^ thisBit) ^ other._binRepr[i]));
     }
-    return result;
+    return *this;
+}
+
+//TODO: test it and check that _carryBit is set right in cases of overflow
+template<std::size_t N>
+BigUnsignedInt<N>& BigUnsignedInt<N>::operator*=(const BigUnsignedInt& other)
+{
+    BigUnsignedInt thisCopy(*this);
+    *this = BigUnsignedInt();
+    std::size_t prev = N - 1; // position of the end of number after last shift
+
+    for (std::size_t i = N - 1; i != std::numeric_limits<BigUnsignedInt::size_type>::max(); --i)
+    {
+        if (other._binRepr[i] == 1)
+        {
+            // Shift the thisCopy so the last digit of original *this stays in i-th place
+            thisCopy <<= prev - i;
+            //if(BigUnsignedInt::_carryFlag == 1) // check that no overflow happened
+            //return *this;
+
+            *this += thisCopy;
+            //if(BigUnsignedInt::_carryFlag == 1)
+            //return *this;
+
+            prev = i;
+        }
+    }
+
+    return *this;
 }
 
 template<std::size_t N>
